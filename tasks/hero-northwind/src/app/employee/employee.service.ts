095d0b2cd51f3166bc45cb0a03792f1f83ba9a4b
realmikesolo <aeryoff@gmail.com>
2022-12-23 22:06:43 +0100

    add: employee module & refactor

---
+++ tasks/hero-northwind/src/app/employee/employee.service.ts
@@ -0,0 +1,15 @@
+import { Env } from '../../env';
+import EmployeeModel from '../../models/employee';
+
+export class EmployeeService {
+  public async getEmployees(page: number): Promise<EmployeeModel[]> {
+    return EmployeeModel.findAll({
+      limit: Env.PAGE_LIMIT,
+      offset: (page - 1) * Env.PAGE_LIMIT,
+    });
+  }
+
+  public async getEmployeesCount(): Promise<number> {
+    return EmployeeModel.count();
+  }
+}
--- tasks/hero-northwind/src/routes/employees.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import { EmployeesHandler } from '../handlers/employees';
-import { getEmployeesOpts } from '../schemas/get-employees';
-
-export const employeesRoutes = async (fastify: FastifyInstance): Promise<void> => {
-  fastify.get('/employees', getEmployeesOpts, (res, rep) =>
-    new EmployeesHandler().getEmployees(res, rep),
-  );
-};
