43e5453a70f77a1ea62b9ddc6370f0d993ccc255
realmikesolo <aeryoff@gmail.com>
2022-12-27 13:24:37 +0100

    add: product module

---
+++ tasks/hero-northwind/src/app/product/product.router.ts
@@ -0,0 +1,62 @@
+import { Static, Type } from '@sinclair/typebox';
+import { FastifyInstance, FastifyRequest } from 'fastify';
+import ProductModel from '../../models/product';
+import { ProductController } from './product.controller';
+
+const productController = new ProductController();
+
+const GetProductsOptions = {
+  schema: {
+    querystring: Type.Object({ page: Type.Number() }),
+  },
+};
+
+const GetProductOptions = {
+  schema: {
+    querystring: Type.Object({ id: Type.Number() }),
+  },
+};
+
+export async function productRouter(fastify: FastifyInstance): Promise<void> {
+  fastify.get('/products', GetProductsOptions, (req: GetProductsRequest, res) =>
+    productController.getProducts(req, res),
+  );
+
+  fastify.get('/product', GetProductOptions, (req: GetProductRequest, res) =>
+    productController.getProduct(req, res),
+  );
+}
+
+export type GetProductsRequest = FastifyRequest<{
+  Querystring: Static<typeof GetProductsOptions['schema']['querystring']>;
+}>;
+
+export type GetProductRequest = FastifyRequest<{
+  Querystring: Static<typeof GetProductOptions['schema']['querystring']>;
+}>;
+
+export type GetProductsResponse = {
+  page: number;
+  pages: number;
+  items: number;
+  total: number;
+  products: Array<Omit<Product, 'supplierName'>>;
+};
+
+export type GetProductResponse = {
+  product: Product;
+};
+
+export type Product = Pick<
+  ProductModel,
+  | 'productID'
+  | 'productName'
+  | 'supplierID'
+  | 'categoryID'
+  | 'quantityPerUnit'
+  | 'unitPrice'
+  | 'unitsInStock'
+  | 'unitsOnOrder'
+  | 'reorderLevel'
+  | 'discontinued'
+> & { supplierName: string };
--- tasks/hero-northwind/src/server.ts
+++ tasks/hero-northwind/src/server.ts
@@ -1,12 +1,13 @@
 import Fastify from 'fastify';
 import { employeeRouter } from './app/employee/employee.router';
+import { productRouter } from './app/product/product.router';
 import { supplierRouter } from './app/supplier/supplier.router';
 import { Env } from './env';
 
 export async function startServer(port = Env.SERVER_PORT): Promise<void> {
   const fastify = Fastify();
 
-  fastify.register(employeeRouter).register(supplierRouter);
+  fastify.register(employeeRouter).register(supplierRouter).register(productRouter);
   await fastify.listen({ port });
 
   console.log(`Server started on ${port} port`);
