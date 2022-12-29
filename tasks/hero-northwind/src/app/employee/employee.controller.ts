9d32eb3504952ab99a3dee8c3a1b92428163d460
realmikesolo <aeryoff@gmail.com>
2022-12-29 23:36:16 +0100

    refactor employee

--- tasks/hero-northwind/src/app/employee/employee.controller.ts
+++ tasks/hero-northwind/src/app/employee/employee.controller.ts
@@ -1,5 +1,6 @@
 import { FastifyReply } from 'fastify';
 import { Env } from '../../env';
+import EmployeeModel from '../../models/employee';
 import {
   GetEmployeeRequest,
   GetEmployeeResponse,
@@ -25,8 +26,6 @@ export class EmployeeController {
       items: Env.PAGE_LIMIT,
       total: rows,
       employees: employees.map((employee) => {
-        employee = employee.toJSON();
-
         return {
           employeeID: employee.employeeID,
           lastName: employee.lastName,
@@ -59,29 +58,29 @@ export class EmployeeController {
       return res.status(404).send({ message: 'Employee not found' });
     }
 
-    const employeeJSON = employee.toJSON();
+    const employeeReportsTo: EmployeeModel | null = employee.getDataValue('employee');
 
     const response: GetEmployeeResponse = {
       employee: {
-        reportID: employeeJSON.employee?.employeeID ?? null,
-        reportFirstName: employeeJSON.employee?.firstName ?? null,
-        reportLastName: employeeJSON.employee?.lastName ?? null,
-        employeeID: employeeJSON.employeeID,
-        lastName: employeeJSON.lastName,
-        firstName: employeeJSON.firstName,
-        title: employeeJSON.title,
-        titleOfCourtesy: employeeJSON.titleOfCourtesy,
-        birthDate: employeeJSON.birthDate,
-        hireDate: employeeJSON.hireDate,
-        address: employeeJSON.address,
-        city: employeeJSON.city,
-        region: employeeJSON.region,
-        postalCode: employeeJSON.postalCode,
-        country: employeeJSON.country,
-        homePhone: employeeJSON.homePhone,
-        extension: employeeJSON.extension,
-        notes: employeeJSON.notes,
-        reportsTo: employeeJSON.reportsTo,
+        reportID: employeeReportsTo?.employeeID ?? null,
+        reportFirstName: employeeReportsTo?.firstName ?? null,
+        reportLastName: employeeReportsTo?.lastName ?? null,
+        employeeID: employee.employeeID,
+        lastName: employee.lastName,
+        firstName: employee.firstName,
+        title: employee.title,
+        titleOfCourtesy: employee.titleOfCourtesy,
+        birthDate: employee.birthDate,
+        hireDate: employee.hireDate,
+        address: employee.address,
+        city: employee.city,
+        region: employee.region,
+        postalCode: employee.postalCode,
+        country: employee.country,
+        homePhone: employee.homePhone,
+        extension: employee.extension,
+        notes: employee.notes,
+        reportsTo: employee.reportsTo,
       },
     };
 
