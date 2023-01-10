e144e9603fd316628621c9517f6a7671196987a6
realmikesolo <aeryoff@gmail.com>
2023-01-10 22:26:51 +0100

    add: supplier logs

--- tasks/hero-northwind/src/app/supplier/supplier.service.ts
+++ tasks/hero-northwind/src/app/supplier/supplier.service.ts
@@ -1,19 +1,52 @@
+import { Log } from '../../db';
 import { Env } from '../../env';
 import SupplierModel from '../../models/supplier';
 
 export class SupplierService {
-  public async getSuppliers(page: number): Promise<SupplierModel[]> {
-    return SupplierModel.findAll({
+  public async getSuppliers(page: number): Promise<{ data: SupplierModel[]; log: Log }> {
+    let query!: string;
+    let duration!: number;
+
+    const data = await SupplierModel.findAll({
       limit: Env.PAGE_LIMIT,
       offset: (page - 1) * Env.PAGE_LIMIT,
+      benchmark: true,
+      logging: (req: string, ms: number) => {
+        query = req;
+        duration = ms;
+      },
     });
+
+    return { data, log: { query, duration: `${duration}ms`, ts: new Date().toISOString() } };
   }
 
-  public async getSuppliersCount(): Promise<number> {
-    return SupplierModel.count();
+  public async getSuppliersCount(): Promise<{ data: number; log: Log }> {
+    let query!: string;
+    let duration!: number;
+
+    const data = await SupplierModel.count({
+      benchmark: true,
+      logging: (req: string, ms: number) => {
+        query = req;
+        duration = ms;
+      },
+    });
+
+    return { data, log: { query, duration: `${duration}ms`, ts: new Date().toISOString() } };
   }
 
-  public async getSupplier(id: number): Promise<SupplierModel | null> {
-    return SupplierModel.findByPk(id);
+  public async getSupplier(id: number): Promise<{ data: SupplierModel | null; log: Log }> {
+    let query!: string;
+    let duration!: number;
+
+    const data = await SupplierModel.findByPk(id, {
+      benchmark: true,
+      logging: (req: string, ms: number) => {
+        query = req;
+        duration = ms;
+      },
+    });
+
+    return { data, log: { query, duration: `${duration}ms`, ts: new Date().toISOString() } };
   }
 }
